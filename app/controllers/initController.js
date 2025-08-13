3ccb22c2126e5ed6a1bebda6fe204d7d76be022f
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2025-08-14 00:23:13 +0200

    initController.js

---
+++ app/controllers/initController.js
@@ -0,0 +1,18 @@
+"use strict";
+import { Voices } from "../../modules/voices.js";
+import { Speech } from "../../modules/speech.js";
+import { Config } from "../../modules/config.js";
+import { Utils } from "../../modules/utils.js";
+import { Storage } from "../../modules/storage.js";
+import { createUI } from "../../modules/ui.js";
+import { createHandlers } from "../../modules/handlers.js";
+import { createKeyboard } from "../../modules/keyboard.js";
+import { createWakeLock } from "../../modules/wakelock.js";
+import { createApp } from "../app.js";
+import { createState } from "../state.js";
+import { createNLTApp } from "../nltapp.js";
+
+export function init() {
+  const NLTApp = createNLTApp({ Voices, Speech, Config, Utils, Storage, createUI, createHandlers, createKeyboard, createWakeLock, createApp, createState });
+  NLTApp.init();
+}
